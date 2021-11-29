a5983550df4ca163bed37f3c98320c5c0879e841
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-29 12:09:23 +0200

    added SmartSocket service interface

---
+++ Module2HW6/ElectricalAppliances/Services/Abstractions/ISmartSocketService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using ElectricalAppliances.Models;
+
+namespace ElectricalAppliances.Services.Abstractions
+{
+    public interface ISmartSocketService
+    {
+        public ElectricalAppliance[] ElectricalAppliances { get; }
+
+        public double CountPowerConsumption();
+
+        public void SortElectricalAppliances(IComparer comparer);
+
+        public void PlugInAppliances(ElectricalAppliance[] electricalAppliances);
+    }
+}
