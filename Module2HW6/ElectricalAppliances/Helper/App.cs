6fdb6235052fee9b1cde2640658b62a43bcecf2b
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-29 12:06:05 +0200

    added App

---
+++ Module2HW6/ElectricalAppliances/Helper/App.cs
@@ -0,0 +1,27 @@
+﻿using ElectricalAppliances.Services.Abstractions;
+
+namespace ElectricalAppliances.Helper
+{
+    public class App
+    {
+        private readonly IElectricalApplianceBuyingService _electricalApplianceBuyingService;
+
+        private readonly ISmartSocketService _smartSocketService;
+
+        public App(IElectricalApplianceBuyingService electricalApplianceBuyingService, ISmartSocketService smartSocketService)
+        {
+            _electricalApplianceBuyingService = electricalApplianceBuyingService;
+            _smartSocketService = smartSocketService;
+        }
+
+        public void SimulateWork()
+        {
+            var comparer = new ElectricalApplianceComparer();
+            var devices = _electricalApplianceBuyingService.GetDevices();
+            _smartSocketService.PlugInAppliances(devices);
+            var totalPowerConsumption = _smartSocketService.CountPowerConsumption();
+            _smartSocketService.SortElectricalAppliances(comparer);
+            var appliance = _smartSocketService.ElectricalAppliances.FindApplianceByCriteria("TefalBlender", 0.4);
+        }
+    }
+}
