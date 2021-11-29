9664bc1affe021a65e99cb5350cf36f1609083c8
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-29 12:11:23 +0200

    added BuyingService

---
+++ Module2HW6/ElectricalAppliances/Services/ElectricalApplianceBuyingService.cs
@@ -0,0 +1,21 @@
+﻿using ElectricalAppliances.Models;
+using ElectricalAppliances.Providers.Abstractions;
+using ElectricalAppliances.Services.Abstractions;
+
+namespace ElectricalAppliances.Services
+{
+    public class ElectricalApplianceBuyingService : IElectricalApplianceBuyingService
+    {
+        private readonly IElectricalApplianceBuyingProvider _electricalApplianceBuyingProvider;
+
+        public ElectricalApplianceBuyingService(IElectricalApplianceBuyingProvider electricalApplianceBuyingProvider)
+        {
+            _electricalApplianceBuyingProvider = electricalApplianceBuyingProvider;
+        }
+
+        public ElectricalAppliance[] GetDevices()
+        {
+            return _electricalApplianceBuyingProvider.GetDevices();
+        }
+    }
+}
