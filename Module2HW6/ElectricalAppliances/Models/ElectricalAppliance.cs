62031b2c7498a8dba621846c0df6ca73d5e36f04
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-29 12:05:15 +0200

    added ElectricalAppliance model

---
+++ Module2HW6/ElectricalAppliances/Models/ElectricalAppliance.cs
@@ -0,0 +1,15 @@
+﻿namespace ElectricalAppliances.Models
+{
+    public abstract class ElectricalAppliance
+    {
+        public double Price { get; set; }
+
+        public abstract string Model { get; }
+
+        public abstract string Type { get; }
+
+        public abstract double PowerConsumptionInkWh { get; }
+
+        public abstract bool IsPortable { get; }
+    }
+}
