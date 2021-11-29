0fea5e3f2ac264fd9a8ed41c4a813424068719b7
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-29 12:06:20 +0200

    added comparer

---
+++ Module2HW6/ElectricalAppliances/Helper/ElectricalApplianceComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using ElectricalAppliances.Models;
+
+namespace ElectricalAppliances.Helper
+{
+    public class ElectricalApplianceComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            var firstItem = x as ElectricalAppliance;
+            var secondItem = y as ElectricalAppliance;
+            return firstItem.PowerConsumptionInkWh.CompareTo(secondItem.PowerConsumptionInkWh);
+        }
+    }
+}
