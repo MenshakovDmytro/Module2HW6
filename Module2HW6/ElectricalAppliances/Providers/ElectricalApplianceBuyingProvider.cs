a0cca9fb69fb493bb21a9bacf073fbdb68b5b02f
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-29 12:07:44 +0200

    added provider

---
+++ Module2HW6/ElectricalAppliances/Providers/ElectricalApplianceBuyingProvider.cs
@@ -0,0 +1,23 @@
+﻿using ElectricalAppliances.Models;
+using ElectricalAppliances.Models.MajorAppliances.CookingEquipment;
+using ElectricalAppliances.Models.MajorAppliances.RefregerationEquipment;
+using ElectricalAppliances.Models.SmallAppliances.CookingEquipment;
+using ElectricalAppliances.Providers.Abstractions;
+
+namespace ElectricalAppliances.Providers
+{
+    public class ElectricalApplianceBuyingProvider : IElectricalApplianceBuyingProvider
+    {
+        public ElectricalAppliance[] GetDevices()
+        {
+            var appliances = new ElectricalAppliance[6];
+            appliances[0] = new BoschWallOven() { Price = 10000 };
+            appliances[1] = new GorenjeRange() { Price = 10888 };
+            appliances[2] = new BoschRefrigerator() { Price = 15800 };
+            appliances[3] = new CuisinartFoodProcessor() { Price = 4890 };
+            appliances[4] = new TefalBlender() { Price = 7000 };
+            appliances[5] = new TefalBlender() { Price = 7000 };
+            return appliances;
+        }
+    }
+}
