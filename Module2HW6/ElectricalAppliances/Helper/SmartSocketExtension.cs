b07d6a0f16243f09e3c003038f60392c477a0378
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-29 12:06:30 +0200

    added extension class

---
+++ Module2HW6/ElectricalAppliances/Helper/SmartSocketExtension.cs
@@ -0,0 +1,20 @@
+﻿using ElectricalAppliances.Models;
+
+namespace ElectricalAppliances.Helper
+{
+    public static class SmartSocketExtension
+    {
+        public static ElectricalAppliance FindApplianceByCriteria(this ElectricalAppliance[] electricalAppliances, string type, double powerConsumption)
+        {
+            for (var i = 0; i < electricalAppliances.Length; i++)
+            {
+                if (electricalAppliances[i].Type.Equals(type) && electricalAppliances[i].PowerConsumptionInkWh.Equals(powerConsumption))
+                {
+                    return electricalAppliances[i];
+                }
+            }
+
+            return null;
+        }
+    }
+}
