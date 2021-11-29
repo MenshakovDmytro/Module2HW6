ed4382faf34546abf662f8c5ded30b2798a1b8aa
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-29 12:06:12 +0200

    added Starter

---
+++ Module2HW6/ElectricalAppliances/Helper/Starter.cs
@@ -0,0 +1,23 @@
+﻿using ElectricalAppliances.Providers;
+using ElectricalAppliances.Providers.Abstractions;
+using ElectricalAppliances.Services;
+using ElectricalAppliances.Services.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ElectricalAppliances.Helper
+{
+    public class Starter
+    {
+        public void Run()
+        {
+            var start = new ServiceCollection()
+                .AddTransient<IElectricalApplianceBuyingProvider, ElectricalApplianceBuyingProvider>()
+                .AddTransient<IElectricalApplianceBuyingService, ElectricalApplianceBuyingService>()
+                .AddTransient<ISmartSocketService, SmartSocketService>()
+                .AddTransient<App>()
+                .BuildServiceProvider();
+            var app = start.GetService<App>();
+            app.SimulateWork();
+        }
+    }
+}
