d12f5c36129ac12c1c036d255d75e16bc58055f0
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-29 12:08:56 +0200

    added service interface for receiving devices

---
+++ Module2HW6/ElectricalAppliances/Services/Abstractions/IElectricalApplianceBuyingService.cs
@@ -0,0 +1,9 @@
+﻿using ElectricalAppliances.Models;
+
+namespace ElectricalAppliances.Services.Abstractions
+{
+    public interface IElectricalApplianceBuyingService
+    {
+        public ElectricalAppliance[] GetDevices();
+    }
+}
